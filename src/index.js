95504f9224b86931da974dacf2bfbeef351de8f6
Dave Kanter <davekanter@Daves-MacBook-Pro.local>
2020-08-10 20:42:25 -0700

    Finished part 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-require("./alert.js")
+import greetings from './robot.js'
+import styles from './app.css'
+var faker = require('faker')
+
+document.write(greetings("Affirmative", "Dave"))
+
+let element = `
+  <div class="${styles.element}">
+    <p>${faker.lorem.paragraph()}</p>
+  </div>
+`
+document.write(element)
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   entry: './src',
   output: {
     path: __dirname + '/build',
     filename: 'bundle.js'
   },
-  mode: 'development'
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader',
+        include: __dirname + '/src'
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        //use: MiniCssExtractPlugin.extract('css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]')
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename: "styles.css"})
+  ]
 }
